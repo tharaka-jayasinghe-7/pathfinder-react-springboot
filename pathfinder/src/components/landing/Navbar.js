f8d033518510058236fb0be700924e0ab91808c5
Tharaka Jayasinghe <tharakajayasinghe7@gmail.com>
2024-10-10 20:26:54 +0530

    First Commit
    Landing page

---
+++ pathfinder/src/components/landing/Navbar.js
@@ -0,0 +1,27 @@
+import React from "react";
+import pathfinderLogo from "../../images/landing/logo.png"; // Adjust the path according to your project structure
+
+const Navbar = () => {
+  return (
+    <nav className="bg-teal-700 p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <a href="#" className="flex items-center">
+          <img
+            src={pathfinderLogo} // Use the imported image here
+            alt="Pathfinder Logo"
+            className="h-10" // Adjust the height as needed
+          />
+        </a>
+        <ul className="flex space-x-4">
+          <li>
+            <a href="#" className="text-white hover:text-gray-400">
+              Admin Login
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
